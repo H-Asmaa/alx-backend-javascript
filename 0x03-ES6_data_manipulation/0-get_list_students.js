97c72aaf03f9aea942dae7cdeca3e73f253c8db2
H-Asmaa <asmaehadar32@gmail.com>
2024-01-09 13:22:48 +0100

    Committing the projects files and 0-get_list_students.js

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+export default function getListStudents(){
+	var students = [
+		{id: 1,firstName: "Guillaume",location: "San Francisco"},
+		{id: 2,firstName: "James",location: "Columbia"},
+		{id: 3,firstName: "Serena",location: "San Francisco"}
+	];
+	return students;
+}
