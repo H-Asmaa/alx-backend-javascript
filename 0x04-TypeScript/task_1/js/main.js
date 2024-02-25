b4d860b45ab4a31b044d642515d75f089897d828
H-Asmaa <asmaehadar32@gmail.com>
2024-02-25 04:11:08 +0100

    Committing task 1 code.

---
+++ 0x04-TypeScript/task_1/js/main.js
@@ -0,0 +1,20 @@
+var teacher3 = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false
+};
+console.log(teacher3);
+var director1 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17
+};
+console.log(director1);
+var printTeacher = function (firstName, lastName) {
+    return "".concat(firstName[0], ". ").concat(lastName);
+};
+console.log(printTeacher("John", "Doe"));
---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,75 @@
+// This file contains four tasks.
+// 1. Let's build a Teacher interface
+interface Teacher {
+	firstName: string,
+	fullTimeEmployee: boolean,
+	lastName: string,
+	yearsOfExperience?: number, // ? defines an optional attribute.
+	location: string,
+	[key: string]: any;
+}
+
+const teacher3: Teacher = {
+	firstName: 'John',
+	fullTimeEmployee: false,
+	lastName: 'Doe',
+	location: 'London',
+	contract: false,
+};
+
+
+console.log(teacher3);
+
+// 2. Extending the Teacher class
+
+interface Directors extends Teacher {
+	numberOfReports: number
+}
+
+const director1: Directors = {
+	firstName: 'John',
+	lastName: 'Doe',
+	location: 'London',
+	fullTimeEmployee: true,
+	numberOfReports: 17,
+};
+
+console.log(director1);
+
+// 3. Printing teachers
+
+interface PrintTeacherFunction {
+	(firstName: string, lastName: string): string;
+}
+
+const printTeacher: PrintTeacherFunction = (firstName, lastName) => {
+	return `${firstName[0]}. ${lastName}`;
+};
+
+console.log(printTeacher("John", "Doe"));
+
+// 4. Writing a class
+
+interface StudentClassInterface{
+	firstName: string;
+	lastName: string;
+	workOnHomework(): string;
+	displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface{
+	firstName: string;
+	lastName: string;
+	constructor(firstName: string, lastName: string) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	workOnHomework() {
+		return 'Currently working';
+	}
+
+	displayName() {
+		return this.firstName;
+	}
+}
