2277faa862b71a209151008c2d4a86d186dbbe33
H-Asmaa <asmaehadar32@gmail.com>
2024-02-27 17:07:22 +0100

    Committing the function displayMessage.
    A function that display a message.
    
    The process.stdout.write was not accepted by the checker.

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 /* A function that displays a message to the STDOUT. */
-const displayMessage = (line) => process.stdout.write(line);
+const displayMessage = (line) => console.log(line);
 
 module.exports = displayMessage;
