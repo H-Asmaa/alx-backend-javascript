79ec25c7abf88defc49fb1554660898094776d0d
H-Asmaa <asmaehadar32@gmail.com>
2024-02-28 21:23:50 +0100

    Committing updates to fix output fail.

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -9,11 +9,17 @@ describe('calculateNumber', () => {
 		assert.equal(calculateNumber(1.2, 3.7), 5);
 		assert.equal(calculateNumber(1.5, 3.7), 6);
 	});
-	it('Testing odd cases...', () => {
+	it('Testing pairs...', () => {
 		assert.equal(calculateNumber(11.4, 0), 11);
 		assert.equal(calculateNumber(0, 6.5), 7);
+	})
+	it('Testing odd cases...', () => {
 		assert.equal(calculateNumber(3.7), NaN);
 		assert.equal(calculateNumber(), NaN);
+	});
+	it('Testing negatives...', () => {
+		assert.equal(calculateNumber(3, -7), -4);
+		assert.equal(calculateNumber(-1, 2), 1);
 	});
 });
 /* eslint-disable */
