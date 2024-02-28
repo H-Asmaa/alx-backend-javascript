b3396fd654ea282ce90e5533b2f8bd22b3836cc0
H-Asmaa <asmaehadar32@gmail.com>
2024-02-28 22:29:11 +0100

    Committing the unitest for the calculateNumber function.

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -7,15 +7,13 @@ describe('calculateNumber', () => {
     assert.equal(calculateNumber(1, 3), 4);
     assert.equal(calculateNumber(1, 3.7), 5);
     assert.equal(calculateNumber(1.2, 3.7), 5);
-    assert.equal(calculateNumber(1.5, 3.7), 6);
   });
   it('Testing pairs...', () => {
     assert.equal(calculateNumber(11.4, 0), 11);
     assert.equal(calculateNumber(0, 6.5), 7);
   });
   it('Testing odd cases...', () => {
-    assert.equal(calculateNumber(3.7), NaN);
-    assert.equal(calculateNumber(), NaN);
+    assert.equal(calculateNumber(Math.E, Math.PI), 6);
   });
   it('Testing negatives...', () => {
     assert.equal(calculateNumber(3, -7), -4);
