13272967a02108d718c4833a867e81b9a3c64ee6
H-Asmaa <asmaehadar32@gmail.com>
2024-02-28 01:52:04 +0100

    Committing a small HTTP sever using express module.
    Working on fixing output fail.

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -2,7 +2,7 @@
 /* Creating a small HTTP sever using express module. */
 const express = require('express');
 
-const app = express()
+const app = express();
 const port = 1245;
 
 app.get('/', (req, res) => {
