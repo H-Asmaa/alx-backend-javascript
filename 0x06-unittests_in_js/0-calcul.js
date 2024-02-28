030119974a4d58d4a3e70febe4a7e532bc436e72
H-Asmaa <asmaehadar32@gmail.com>
2024-02-28 21:13:19 +0100

    Committing updates to fix output fail.

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
 /* A function that rounds a and b and returns the sum of it. */
-module.exports =  calculateNumber = (a, b) => Math.round(a) + Math.round(b);
+const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
+module.exports = calculateNumber;
