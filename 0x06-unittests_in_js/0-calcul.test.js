2ee13340906c140e61f53243cb72a5710c706999
H-Asmaa <asmaehadar32@gmail.com>
2024-02-28 22:25:27 +0100

    Committing the unitest for the calculateNumber function.

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,19 +3,23 @@ const calculateNumber = require('./0-calcul');
 
 /* eslint-disable */
 describe('calculateNumber', () => {
-  it('normal number', () => {
+  it('Testing normal cases...', () => {
     assert.equal(calculateNumber(1, 3), 4);
-  });
-  it('zahlen number', () => {
-    assert.equal(calculateNumber(1, -3), -2);
-  });
-  it('radical number', () => {
     assert.equal(calculateNumber(1, 3.7), 5);
     assert.equal(calculateNumber(1.2, 3.7), 5);
     assert.equal(calculateNumber(1.5, 3.7), 6);
   });
-  it('non-radical number', () => {
-    assert.equal(calculateNumber(Math.E, Math.PI), 6);
+  it('Testing pairs...', () => {
+    assert.equal(calculateNumber(11.4, 0), 11);
+    assert.equal(calculateNumber(0, 6.5), 7);
+  });
+  it('Testing odd cases...', () => {
+    assert.equal(calculateNumber(3.7), NaN);
+    assert.equal(calculateNumber(), NaN);
+  });
+  it('Testing negatives...', () => {
+    assert.equal(calculateNumber(3, -7), -4);
+    assert.equal(calculateNumber(-1, 2), 1);
   });
 });
-/* eslint-enable */
+/* eslint-disable */
