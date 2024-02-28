95c1bcffe952bbfa17883a6d63fef2a2d39a84b6
H-Asmaa <asmaehadar32@gmail.com>
2024-02-28 20:06:29 +0100

    Committing yet another trial to fix output fail.

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,29 +1,17 @@
 const calculateNumber = require("./0-calcul.js");
 const assert = require('assert');
 
-describe('calculateNumber', function(){
-	it('...', function(){
+describe('calculateNumber', () => {
+	it('Testing normal cases...', function(){
 		assert.equal(calculateNumber(1, 3), 4);
-	});
-	it('...', function(){
 		assert.equal(calculateNumber(1, 3.7), 5);
-	});
-	it('...', function(){
 		assert.equal(calculateNumber(1.2, 3.7), 5);
-	});
-	it('...', function(){
 		assert.equal(calculateNumber(1.5, 3.7), 6);
 	});
-	it('...', function(){
+	it('Testing odd cases...', () => {
 		assert.equal(calculateNumber(11.4, 0), 11);
-	});
-	it('...', function(){
 		assert.equal(calculateNumber(0, 6.5), 7);
-	});
-	it('...', function(){
 		assert.equal(calculateNumber(3.7), NaN);
-	});
-	it('...', function(){
 		assert.equal(calculateNumber(), NaN);
 	});
 });
