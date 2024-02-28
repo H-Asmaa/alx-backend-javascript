d73adb49d964b0ae0c5234292359e826ecf46c4f
H-Asmaa <asmaehadar32@gmail.com>
2024-02-28 07:34:29 +0100

    Committing modification to the variables inside the function taskBlock so that the variables aren’t overwritten inside the conditional block.

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/* Modifying modify the variables inside the function taskBlock so that
+the variables aren’t overwritten inside the conditional block. */
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    /* eslint-disable */
+    const task = true;
+    const task2 = false;
+  }
+
+  return [task, task2];
+}
