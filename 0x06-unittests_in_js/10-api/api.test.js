671a5dab6aa5021b8a997fa52c5c404f07597c82
H-Asmaa <asmaehadar32@gmail.com>
2024-03-01 00:06:39 +0100

    Committing an API with two routes and the tests for each.
    I fixed the tests for the previous tasks.
    
    Upgrading the code. Amazing advices from a friend.

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -8,7 +8,7 @@ const url = 'http://localhost:7865';
 
 describe('apiTesting /', () => {
   it('Testing correct statusCode...', (done) => {
-    request('http://localhost:7865/', (error, response) => {
+    request(`${url}`, (error, response) => {
       expect(response.statusCode).to.equal(200);
       expect(response.body).to.equal('Welcome to the payment system');
       const consoleLog = sinon.spy(console, 'log');
@@ -21,20 +21,20 @@ describe('apiTesting /', () => {
 
 describe('apiTesting /cart/:ip', () => {
   it('Testing correct id type...', (done) => {
-    request('http://localhost:7865/cart/1', (error, response) => {
+    request(`${url}/cart/1`, (error, response) => {
       expect(response.statusCode).to.equal(200);
       expect(response.body).to.equal('Payment methods for cart 1');
       done();
     });
   });
   it('Testing wrong id type...', (done) => {
-    request('http://localhost:7865/cart/hello', (error, response) => {
+    request(`${url}/cart/hello`, (error, response) => {
       expect(response.statusCode).to.equal(404);
       done();
     });
   });
   it('Testing correct content...', (done) => {
-    request('http://localhost:7865/cart', (error, response) => {
+    request(`${url}/cart`, (error, response) => {
       expect(response.statusCode).to.equal(404);
       done();
     });
