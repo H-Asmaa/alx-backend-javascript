778b212343deda46c5a18b5a09b6c66298d7cb51
H-Asmaa <asmaehadar32@gmail.com>
2024-02-29 23:57:28 +0100

    Committing an API with two routes and the tests for each.

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -6,18 +6,8 @@ const sinon = require('sinon');
 describe('apiTesting /', () => {
   it('Testing correct statusCode...', (done) => {
     request('http://localhost:7865/', (error, response) => {
-      expect(response.status, 200);
-      done();
-    });
-  });
-  it('Testing correct result...', (done) => {
-    request('http://localhost:7865/', (error, response) => {
-      expect(response.body).to.deep.equal('Welcome to the payment system');
-      done();
-    });
-  });
-  it('Testing correct log...', (done) => {
-    request('http://localhost:7865/', (error, response) => {
+      expect(response.statusCode).to.equal(200);
+      expect(response.body).to.equal('Welcome to the payment system');
       const consoleLog = sinon.spy(console, 'log');
       expect(consoleLog.calledWith('API available on localhost port 7865'));
       consoleLog.restore();
@@ -29,25 +19,20 @@ describe('apiTesting /', () => {
 describe('apiTesting /cart/:ip', () => {
   it('Testing correct id type...', (done) => {
     request('http://localhost:7865/cart/1', (error, response) => {
-      expect(response.status, 200);
+      expect(response.statusCode).to.equal(200);
+      expect(response.body).to.equal('Payment methods for cart 1');
       done();
     });
   });
   it('Testing wrong id type...', (done) => {
     request('http://localhost:7865/cart/hello', (error, response) => {
-      expect(response.status, 404);
-      done();
-    });
-  });
-  it('Testing correct content...', (done) => {
-    request('http://localhost:7865/cart/1', (error, response) => {
-      expect(response.body).to.equal('Payment methods for cart 1');
+      expect(response.statusCode).to.equal(404);
       done();
     });
   });
   it('Testing correct content...', (done) => {
     request('http://localhost:7865/cart', (error, response) => {
-      expect(response.status, 404);
+      expect(response.statusCode).to.equal(404);
       done();
     });
   });
