e3826d762552aec72a7aee1be73516370b063471
H-Asmaa <asmaehadar32@gmail.com>
2024-02-29 01:13:18 +0100

    Committing a function that checks the type passed and applies the operation.
    I had a typo in DIVIDE everywhere.
    
    I had to check that the rounded value of b is zero, not b itself.

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -6,7 +6,7 @@ module.exports = (type, a, b) => {
     case 'SUBTRACT':
       return Math.round(a) - Math.round(b);
     case 'DIVIDE':
-      if (b === 0) return 'Error';
+      if (Math.round(b) === 0) return 'Error';
       return Math.round(a) / Math.round(b);
   }
 };
