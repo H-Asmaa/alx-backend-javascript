2eb9f2ca0f32e848aba3a23a5bbb6ee9ec75062a
H-Asmaa <asmaehadar32@gmail.com>
2024-02-29 01:02:35 +0100

    Committing a function that checks the type passed and applies the operation.

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+/* eslint-disable */
+describe('calculateNumber', () => {
+	it('Testing normal cases...', () => {
+	  assert.equal(calculateNumber('SUM', 1, 3), 4);
+	  assert.equal(calculateNumber('SUBTRACT', 5, 3.7), 1);
+	  assert.equal(calculateNumber('DEVIDE', 8.2, 3.7), 2);
+	});
+	it('Testing devision on zero...', () => {
+		assert.equal(calculateNumber('DEVIDE', 1, 0), 'Error');
+	});
+	it('Testing odds...', () => {
+		assert.equal(calculateNumber('SUM', Math.E, Math.PI), 6);
+		assert.equal(calculateNumber('SUBTRACT', Math.E, Math.PI), 0);
+		assert.equal(calculateNumber('DEVIDE', Math.E, Math.PI), 1);
+	});
+	it('Testing negatives...', () => {
+		assert.equal(calculateNumber('SUM', -1, 3), 2);
+		assert.equal(calculateNumber('SUBTRACT', 5, -3.7), 9);
+	});
+  });
+/* eslint-disable */
