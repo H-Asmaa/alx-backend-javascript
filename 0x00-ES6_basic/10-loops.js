413c457b7cb874957cc01adefc57a88749c59456
H-Asmaa <asmaehadar32@gmail.com>
2024-03-01 17:56:00 +0100

    COmmitting a function that appends a string a string to each value of a table.

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+export default function appendToEachArrayValue(array, appendString) {
+  const tmp = [];
+  for (const idx of array) tmp.push(appendString + idx);
+
+  return tmp;
+}
