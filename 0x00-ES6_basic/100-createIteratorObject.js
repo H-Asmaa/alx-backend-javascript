49f10dd299028e1dc796b009a435c6a29a21f037
H-Asmaa <asmaehadar32@gmail.com>
2024-03-01 18:57:06 +0100

    Committing a function that gets the employees names from an allEployees object.

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,6 @@
+export default function createIteratorObject(report) {
+  const employeesList = [];
+  for (const dep of Object.values(report.allEmployees))
+    for (const employeeName of dep) employeesList.push(employeeName);
+  return employeesList;
+}
