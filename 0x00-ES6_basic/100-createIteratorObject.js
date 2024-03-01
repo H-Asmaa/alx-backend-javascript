98dbed0f2dfeed2720521e81ce3fadad383fab18
H-Asmaa <asmaehadar32@gmail.com>
2024-03-01 18:58:05 +0100

    Committing a function that gets the employees names from an allEployees object.
    Fixing requirement fail, eslint.

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,6 +1,7 @@
 export default function createIteratorObject(report) {
   const employeesList = [];
-  for (const dep of Object.values(report.allEmployees))
+  for (const dep of Object.values(report.allEmployees)) {
     for (const employeeName of dep) employeesList.push(employeeName);
+  }
   return employeesList;
 }
