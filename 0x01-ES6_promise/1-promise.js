aacb3fe693bf15ee19df58f69c555d9e4d77c890
H-Asmaa <asmaehadar32@gmail.com>
2024-03-03 22:05:29 +0100

    Committing a function that returns a promise.

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+export default (success) => new Promise((resolve, reject) => {
+  if (success === true) {
+    resolve({ status: 200, body: 'Success' });
+  } else {
+    reject(new Error('The fake API is not working currently'));
+  }
+});
