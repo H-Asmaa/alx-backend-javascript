3236ae5f8c0b87af48883c64e5f360a015461272
H-Asmaa <asmaehadar32@gmail.com>
2024-03-03 17:46:42 +0100

    Committing a function named updateUniqueItems that returns an updated map for all items with initial quantity at 1.
    Fixing output fail.

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,5 +1,5 @@
 export default function updateUniqueItems(map) {
-  if (map instanceof Map) throw new Error('Cannot process');
+  if (!(map instanceof Map)) throw new Error('Cannot process');
   for (const [key, val] of map.entries()) {
     if (val === 1) map.set(key, 100);
   }
--- 0x03-ES6_data_manipulation/MAIN/10-main.js
+++ 0x03-ES6_data_manipulation/MAIN/10-main.js
@@ -1,10 +1,13 @@
-import updateUniqueItems from "../10-update_uniq_items.js";
-import groceriesList from "../9-groceries_list.js";
+import updateUniqueItems from '../10-update_uniq_items.js';
+import groceriesList from '../9-groceries_list.js';
 
 const map = groceriesList();
 console.log(map);
 
-updateUniqueItems(map)
+updateUniqueItems(map);
 console.log(map);
 
 console.log(updateUniqueItems([]));
+
+const map0 = groceriesList();
+console.log(updateUniqueItems(map0));
