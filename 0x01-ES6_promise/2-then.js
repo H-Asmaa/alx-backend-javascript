4c35de38681ec399195c920dd8b59f6c1e94efa2
H-Asmaa <asmaehadar32@gmail.com>
2024-03-03 22:26:11 +0100

    Committing a function that resolves a promise.
    Fixing output fail.

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 export default (promise) => new Promise((resolve, reject) => {
-  if (promise) {
-    console.log('Got a response from the API');
-    resolve({ status: 200, body: 'Success' });
-  } else {
-    reject(new Error());
-  }
+  promise
+    .then(() => {
+      console.log('Got a response from the API');
+      resolve({ status: 200, body: 'Success' });
+    })
+    .catch((error) => {
+      reject(error);
+    });
 });
