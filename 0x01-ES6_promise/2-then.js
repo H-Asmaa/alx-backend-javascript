b99ebab2416d888d02b55439e9218aba7541e5f6
H-Asmaa <asmaehadar32@gmail.com>
2024-03-16 02:30:36 +0000

    Committing updated code for task 2.

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,7 +3,7 @@ export default (promise) => new Promise((resolve, reject) => {
   promise
     .then(() => {
       console.log('Got a response from the API');
-      resolve({ status: 200, body: 'Success' });
+      resolve({ status: 200, body: 'success' });
     })
     .catch((error) => {
       reject(error);
