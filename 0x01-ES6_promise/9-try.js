16b894c71516eadd833c4dc3fae755f1d7203dd9
H-Asmaa <asmaehadar32@gmail.com>
2024-03-21 05:22:13 +0000

    Committing a function that creates and return an array named queue

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -3,8 +3,9 @@ export default function guardrail(mathFunction) {
   try {
     queue.push(mathFunction());
   } catch (error) {
-    queue.push(error.message);
+    queue.push(error.toString());
+  } finally {
+    queue.push('Guardrail was processed');
   }
-  queue.push('Guardrail was processed');
   return queue;
 }
