b5fc6bde78ee3fdcc4867293f962bbbc233c119f
H-Asmaa <asmaehadar32@gmail.com>
2024-03-21 01:46:16 +0000

    Committing a function that calls two functions and handles their promises.
    Output fail

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,10 +3,12 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
-	signUpUser(firstName, lastName),
-	uploadPhoto(fileName),
-  ]).then((res) => res.map(({ status, value, reason }) => ({
-	status,
-	value: status === 'fulfilled' ? value : reason.toString(),
-  })));
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((res) =>
+    res.map(({ status, value, reason }) => ({
+      status,
+      value: status === 'fulfilled' ? value : reason.toString(),
+    }))
+  );
 }
