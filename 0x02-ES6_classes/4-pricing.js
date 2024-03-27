f8dfa1e2905e19a7ed8e0f6c8ff4f29a841b6640
H-Asmaa <asmaehadar32@gmail.com>
2024-03-27 06:11:21 +0000

    Committing an implementation of class Pricing.

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,33 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  set amount(value) {
+    this._amount = value;
+  }
+
+  set currency(value) {
+    if (value instanceof Currency) throw TypeError('Currency must be a Currency');
+    this._currency = value;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
