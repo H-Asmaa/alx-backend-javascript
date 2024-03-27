e06ee21e82115e8541e4dee045fbc37ac0d06f90
H-Asmaa <asmaehadar32@gmail.com>
2024-03-27 06:29:11 +0000

    Committing an implementation of class Building.

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,6 +1,6 @@
 export default class Building {
   constructor(sqft) {
-    this.sqft = sqft;
+    this._sqft = sqft;
     if (this.constructor !== Building && this.evacuationWarningMessage === undefined) throw Error('Class extending Building must override evacuationWarningMessage');
   }
 
