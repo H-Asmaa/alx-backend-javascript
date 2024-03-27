2d16e49d11edbb68ae14b53f4d2e5d93190bd91c
H-Asmaa <asmaehadar32@gmail.com>
2024-03-27 05:46:47 +0000

    Committing implementation of class named HolbertonCourse.

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,34 @@
+export default class HolbertonClass {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') throw new TypeError('Name must be a string');
+    else this._name = value;
+  }
+
+  set length(value) {
+    if (typeof value !== 'number') throw new TypeError('Length must be a number');
+    else this._length = value;
+  }
+
+  set students(value) {
+    if (!Array.isArray(value) || !value.every((item) => typeof item === 'string')) throw new TypeError('Students must be an array of strings');
+    else this._students = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+}
